081e69f17398bca3364ed1f2540e153af4bf0248
Vaishnavi Gowda <132700922+Vaishnavigowda55@users.noreply.github.com>
2025-07-01 11:31:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
-from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
-REQUESTS = Counter('http_requests_total', 'Total HTTP Requests')
 
 @app.route('/')
 def home():
-    REQUESTS.inc()
     return "Hello from Python App!"
 
 if __name__ == '__main__':
